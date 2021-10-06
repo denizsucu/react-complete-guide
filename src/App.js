cb9546684fcffa39a836403aac3bc2b386c6e29f
denizsucu <denizsucumzfl@gmail.com>
2021-10-06 23:08:36 +0300

    eventListeners started

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import Expenses from "./components/Expenses";
-import "./components/Expenses/Expenses.css";
-import "./App.css";
-import React from "react";
+import React from 'react';
 
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
-
+const App = () => {
   const expenses = [
     {
-      id: "e1",
-      title: "Paper",
-      amount: 15,
-      date: new Date(2020, 1, 2)
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "e2",
-      title: "Paperclip",
-      amount: "$10",
-      date: new Date(2020, 5, 2)
-    }
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
 
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h2', {}, 'String'),
-    React.createElement(Expenses, {items: expenses})
-  );
-
-  // return (
-  //   <div className="expenses">
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses}/>
-  //   </div>
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,12 +1,11 @@
-import "./Card.css"
+import React from 'react';
 
-function Card(props){
-    const classes = "prop " + props.className;
-    return(
-        <div className={classes}>
-            {props.children}
-        </div>
-    );
-}
+import './Card.css';
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
