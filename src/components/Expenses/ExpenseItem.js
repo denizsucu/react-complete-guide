6c81f44da49e18b20042a940417fb3671414a7e6
denizsucu <denizsucu@gmail.com>
2021-10-07 11:26:12 +0300

    last changes added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,14 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import React from 'react';
+import React, {useState} from 'react';
 
 function ExpenseItem(props){
-
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);
 
     const clickHandler = () =>{
-        title = "Updated!!"
+        setTitle('Updated!');
+        console.log(title)
     }
     
     return(
